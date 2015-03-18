cac7747718051f49238324bcd1fc1e3d614775ce
Jonathan Inloes <jonathan@rivermeadow.com>
2015-03-18 14:17:56 -0700

    added form based login instead of basic auth

--- auth-server/src/main/java/com/jinloes/AuthserverApplication.java
+++ auth-server/src/main/java/com/jinloes/AuthserverApplication.java
@@ -6,7 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders
+        .AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration
+        .WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.configurers
         .ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration
@@ -18,6 +24,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers
         .AuthorizationServerEndpointsConfigurer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -36,6 +43,37 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(AuthserverApplication.class, args);
     }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        //registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+    }
+
+    @Configuration
+    @Order(-10)
+    protected static class LoginConfig extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.formLogin()
+                    .loginPage("/login").permitAll()
+                    .and()
+                    .requestMatchers().antMatchers("/login", "/oauth/authorize",
+                    "/oauth/confirm_access")
+                    .and()
+                    .authorizeRequests().anyRequest().authenticated();
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.parentAuthenticationManager(authenticationManager);
+        }
+    }
+
 
     @Configuration
     @EnableAuthorizationServer
